32b1b644b9c66e36b5af078eb230aeda9594d3f9
helenewegener1 <helene.wegener@gmail.com>
2025-12-04 08:30:50 +0100

    update

--- run_metrics.sh
+++ run_metrics.sh
@@ -6,9 +6,16 @@ script_dir="$(cd -- "$(dirname -- "$0")" && pwd)"
 python_bin="${script_dir}/.venv/bin/python"
 [ -x "$python_bin" ] || python_bin="python"
 
+# "${python_bin}" "${script_dir}/flow_metrics.py" \
+#   --name "dgcytof" \
+#   --output_dir "${script_dir}/out/data/metrics/all/flow_metrics" \
+#   --analysis.prediction "${script_dir}/out/data/analysis/default/dgcytof/dgcytof_predicted_labels.txt" \
+#   --data.true_labels "${script_dir}/out/data/data_preprocessing/default/data_preprocessing_labels.txt" \
+#   --metric "all"
+
 "${python_bin}" "${script_dir}/flow_metrics.py" \
-  --name "dgcytof" \
+  --name "random" \
   --output_dir "${script_dir}/out/data/metrics/all/flow_metrics" \
-  --analysis.prediction "${script_dir}/out/data/analysis/default/dgcytof/dgcytof_predicted_labels.txt" \
-  --data.true_labels "${script_dir}/out/data/data_preprocessing/default/data_preprocessing_labels.txt" \
+  --analysis.prediction "${script_dir}/out/data/analysis/default/random/random_predicted_labels.txt" \
+  --data.true_labels "${script_dir}/out/data/data_preprocessing/default/data_import.test.labels.gz" \
   --metric "all"
